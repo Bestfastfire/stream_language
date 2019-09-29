e428b390d35f923aa6b30801c549b8fa43b5a548
Woton Sampaio <ostdor@hotmail.com>
2019-09-28 21:39:13 -0300

    add example

---
+++ example/flutter_stream_language/lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter_stream_language/stream_language.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final language = LanguageBloc(
+      child: 'languages',
+      defaultLanguage: 'pt_BR',
+      defaultRoute: 'default'
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return FirstLanguageStart(
+      future: language.init(),
+      builder: (c){
+        return StreamLanguage(
+          screenRoute: ['screen-1'],
+          builder: (data, route, def) => Scaffold(
+            appBar: AppBar(
+              title: Text(route['title']),
+            ),
+            body: Center(
+              child: RaisedButton(
+                  child: Text(route['btn']),
+                  onPressed: () => language.showAlertChangeLanguage(
+                      context: context,
+                      title: def['change-language']['title'],
+                      btnNegative: def['change-language']['btn-negative']
+                  )
+              ),
+            ), // This trailing comma makes auto-formatting nicer for build methods.
+          ),
+        );
+      },
+    );
+  }
+}
